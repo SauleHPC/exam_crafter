1ae6bb1539581b925cbc55b6dc376ec48ecc7422
Erik Saule <esaule@charlotte.edu>
2026-02-05 00:29:18 -0500

    towards JSON validation of questions

--- llm_provider.py
+++ llm_provider.py
@@ -1,9 +1,12 @@
 import aisuite as ai
 import httpx
+import json
 
-BASE_URL = "https://cci-llm.charlotte.edu/api/v1"
+#BASE_URL = "https://cci-llm.charlotte.edu/api/v1"
+BASE_URL = "http://localhost:1234/v1"
 API_KEY="dummy"
 
+debug = True
 
 def get_chat_completion(messages):
     provider_configs = {
@@ -23,6 +26,55 @@ def get_chat_completion(messages):
         messages=messages
     )
     return response
+
+def _validate(content_str, template):
+    try:
+        obj = json.loads(content_str)
+        for x in template:
+            obj[x] #tried to generate an exception if malformed
+    except Exception as e:
+        print (f"cant parse as JSON {content_str}")
+        obj = None
+    return obj
+
+
+def get_chat_completion_JSON(messages, template) -> dict:
+    #This returns the object decoded from the answer or None if it can't do that
+    #This does basic checks that the generated JSON follows the structure provided
+    format_message = {"role": "system", "content": f"Provide the answer as a JSON object that follows the following format. Use the exact same fields: {json.dumps(template)}"}
+    local_message = [*messages, format_message]
+
+    nbtry = 3
+    obj = None
+    response = get_chat_completion(local_message)
+
+    content_str = response.choices[0].message.content
+    if debug:
+        print (f"Received: {content_str}")    
+        
+    obj = _validate(content_str, template)
+            
+    if obj == None: #validation failed
+        if debug:
+            print ("Trying to cleanup")
+        reformat_message = [
+            {"role": "system", "content": f"You are about to receive a message. Reformat it as a JSON object that follows this format: {json.dumps(template)}"},
+            {"role": "system", "content": content_str}
+        ]
+        response = get_chat_completion(local_message)
+
+        content_str = response.choices[0].message.content
+        if debug:
+            print (f"Received: {content_str}")    
+            
+        obj = _validate(content_str, template)
+        if debug:
+            if obj == None:
+                print ('cleanup failed')
+            else:
+                print ('cleanup success')
+    
+    return obj
 
 if __name__ == "__main__":
     response = get_chat_completion(
--- sql_crafter.py
+++ sql_crafter.py
@@ -1,5 +1,7 @@
-from llm_provider import get_chat_completion
+from llm_provider import get_chat_completion, get_chat_completion_JSON
+import json
 
+debug = True
 
 def sql_queries_problem(theme:str):
     if theme == None or theme == "":
@@ -8,58 +10,69 @@ def sql_queries_problem(theme:str):
         
     output = {}
     output['theme'] = theme
-    messages =         [
-            {"role": "system", "content": "This is in the context of writing exam questions. If I ask  to give me something; just give me the thing. Don't put any fluff around it. If I ask for description of queries. Don't give me the queries, JUST the prompt that you would give a student. If I ask the same question multiple times, give me a different answer."},
+    base_messages =         [
+            {"role": "system", "content": "This is in the context of writing exam questions. If I ask to give me something; just give me the thing. Don't put any fluff around it. If I ask the same question multiple times, give me a different answer."},
             {"role": "user", "content": "Give me a set of relational tables on the theme of \"{}\". Make sure to include a small sample of data. No more than 4 tables.".format(theme)},
         ]
 
     
-    response = get_chat_completion(messages= messages)
-    messages.append({"role": "assistant", "content":response.choices[0].message.content})
-    #print (response.choices[0].message.content)
+    response = get_chat_completion(messages= base_messages)
+    base_messages.append({"role": "assistant", "content":response.choices[0].message.content})
 
     output["tables"] = response.choices[0].message.content
 
     output["sql_basic"] = []
     
     for i in range (0,nbquestion):
-        messages.append({"role": "user", "content":"Give me the description of a query that could be made on these tables that are basic SELECT FROM WHERE queries; no JOIN or GROUP BY."})
-
-        response = get_chat_completion(messages= messages)
-        messages.append({"role": "assistant", "content":response.choices[0].message.content})
+        messages = base_messages.copy()
+        messages.append({"role": "user", "content":"Give me the prompt of a query that could be made on these tables that are basic SELECT FROM WHERE queries; no JOIN or GROUP BY."},
+ )
+        response = get_chat_completion_JSON(messages= messages, template={"prompt":" Write the SQL query that returns the artifact identifier and its name of more than 3 pounds.", "sql": "SELECT artifactID, name FROM artifacts WHERE weight < 3;"})
+        if response == None:
+            continue
+        
+        messages.append({"role": "assistant", "content":json.dumps(response)})
                 
-        #print (response.choices[0].message.content)
+        if debug:
+            print(response)
+        output["sql_basic"].append( response)
 
-        output["sql_basic"].append( response.choices[0].message.content)
-
+        
     output["sql_join"] = []
     for i in range (0,nbquestion):
+        messages = base_messages.copy()
         messages.append({"role": "user", "content":"Give me the description of a query that could be made on these tables that are SELECT JOIN WHERE queries; no GROUP BY."})
-        response = get_chat_completion(messages= messages)
-        messages.append({"role": "assistant", "content":response.choices[0].message.content})
 
-        #print (response.choices[0].message.content)
+        response = get_chat_completion_JSON(messages= messages, template={"prompt":" Write the SQL query that returns the artifact identifier and its name of more than 3 pounds.", "sql": "SELECT artifactID, name FROM artifacts WHERE weight < 3;"})
+        if response == None:
+            continue
+        messages.append({"role": "assistant", "content":json.dumps(response)})
                 
-        output["sql_join"].append( response.choices[0].message.content)
+        if debug:
+            print(response)
+        output["sql_join"].append( response)
 
     output["sql_groupby"] = []
     for i in range (0,nbquestion):
+        messages = base_messages.copy()
         messages.append({"role": "user", "content":"Give me the description of a query that could be made on these tables that are GROUP BY HAVING queries."})
 
-        response = get_chat_completion(messages= messages)
-        #print (response.choices[0].message.content)
-        messages.append({"role": "assistant", "content":response.choices[0].message.content})
+        response = get_chat_completion_JSON(messages= messages, template={"prompt":" Write the SQL query that returns the artifact identifier and its name of more than 3 pounds.", "sql": "SELECT artifactID, name FROM artifacts WHERE weight < 3;"})
+        if response == None:
+            continue
+        messages.append({"role": "assistant", "content":json.dumps(response)})
+                
+        if debug:
+            print(response)
 
-        output["sql_groupby"].append(response.choices[0].message.content)
+        output["sql_groupby"].append(response)
 
     return output
     
 
-
-
 if __name__ == "__main__":
     my_problem = sql_queries_problem("")
 
-    print(my_problem)
+    print(json.dumps(my_problem, indent=2))
 
     
